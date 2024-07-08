a0b326ccd8c9aecac56cbd095d925b881f02fa3b
tiagobalan <tiagobalan@edu.unifil.br>
2024-07-08 19:20:38 -0300

    first commit - subindo projeto

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var menuIcon = document.querySelector('.menu-icon');
+var ul = document.querySelector('.ul')
+
+menuIcon.addEventListener('click', ()=> {
+    if (ul.classList.contains('ativo')) {
+        ul.classList.remove('ativo');
+        document.querySelector('.menu-icon img').src = 'imgs/menu (1).png'
+    } else {
+        ul.classList.add('ativo')
+        document.querySelector('.menu-icon img').src = 'imgs/close.png'
+
+    }
+})
